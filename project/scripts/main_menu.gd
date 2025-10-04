48d76ac5ccc206cf0747922f825c7aaf9a26f2c0
Nate <nathanberhane2019@gmail.com>
2025-10-04 20:45:00 +0300

    corrections

--- project/scripts/main_menu.gd
+++ project/scripts/main_menu.gd
@@ -12,6 +12,6 @@ func _process(delta: float) -> void:
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
 func _on_quit_pressed() -> void:
 	get_tree().quit()
