63a0066ba64210ff546a19bfc4173893a3180d56
Nate <nathanberhane2019@gmail.com>
2025-10-04 20:40:29 +0300

    new things

--- project/scripts/settings.gd
+++ project/scripts/settings.gd
@@ -1,4 +1,7 @@
 extends Control
-
 func _on_volume_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(0,value)
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
