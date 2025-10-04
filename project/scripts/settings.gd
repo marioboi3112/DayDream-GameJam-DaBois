9804ea5d3decc67dac4555a1d8b30e29c401e4cd
Nate <nathanberhane2019@gmail.com>
2025-10-04 21:06:08 +0300

    added sound

--- project/scripts/settings.gd
+++ project/scripts/settings.gd
@@ -1,5 +1,5 @@
 extends Control
-func _on_volume_value_changed(value: float) -> void:
+func _on_volume_value_changed(_value: float) -> void:
 	pass
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
