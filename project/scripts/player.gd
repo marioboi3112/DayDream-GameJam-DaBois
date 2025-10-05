beadc91bfd820358c31d7dbe9d5079075ff6e571
Nate <nathanberhane2019@gmail.com>
2025-10-05 08:06:27 +0300

    fixed the moving issue

--- project/scripts/player.gd
+++ project/scripts/player.gd
@@ -13,7 +13,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	if is_on_floor():
-		if dir_face == 0:
+		if direction == 0:
 			if is_attacking == false:
 				animated_sprite.play("idle")
 		else:
@@ -21,15 +21,14 @@ func _physics_process(delta):
 	else:
 		animated_sprite.play("jump")
 	if Input.is_action_just_pressed("slash"):
-		animated_sprite.play("attack")
+		animated_sprite.play("slash")
 		is_attacking = true
 	else:
 		is_attacking =false
 	
-	if dir_face > 0 :
+	if direction > 0 :
 		animated_sprite.flip_h = false
-	elif dir_face < 0:
-		
+	elif direction < 0:
 		animated_sprite.flip_h = true
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -37,7 +36,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	if direction:
-		velocity.x = dir_face * SPEED
+		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
