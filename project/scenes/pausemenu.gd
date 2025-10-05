eeb01918125dbc6efe10149decfdcdcbc4c61e46
yafu2113 <beecreep2113@gmail.com>
2025-10-05 09:17:02 +0300

    obi is gay

--- project/scenes/pausemenu.gd
+++ project/scenes/pausemenu.gd
@@ -7,7 +7,7 @@ var paused =  false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("pause"):
+	if Input.is_action_just_pressed("escape"):
 		pausemenu()
 	
 
